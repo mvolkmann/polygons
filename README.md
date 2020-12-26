4667ddbd5c5dc376b78834a5f2cd37a6354b0758
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-12-26 15:43:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# polygons
+
+This will be used to demonstrate using Rust and WebAssembly
+to improve the performance of an JavaScript application.
+The user can create any of number of polygons,
+each containing any number of points.
+They can then rotate the polygons which requires matrix multiplication
+that will be handled by either JavaScript or Rust.
+We will compare the performance differences.
---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  let polygon = [];
+  let polygons = [];
+  let x;
+  let y;
+
+  function savePolygon() {
+    if (polygon.length) polygons.push(polygon);
+    polygon = [];
+  }
+</script>
+
+<main>
+  <form on:submit|preventDefault>
+    <button disabled={polygon.length === 0} on:click={savePolygon}>Save Polygon</button>
+  </form>
+  sv
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
